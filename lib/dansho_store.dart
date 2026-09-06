c87509d4a9531b981dec0b9603b996b85585c8d0
Abduallh Elsaeid <124896053+Abduallh-Abdo@users.noreply.github.com>
2026-09-06 12:52:20 +0300

    first commit

---
+++ lib/dansho_store.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class DanshoStore extends StatelessWidget {
+  const DanshoStore({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dansho Store',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('Dansho Store'),
+        ),
+        body: const Center(child: Text('Hello World')),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:dansho_store/dansho_store.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DanshoStore());
+}
