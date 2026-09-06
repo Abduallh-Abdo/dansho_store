66fa0b6db5dafe53215dd37caad15d721710f134
Abduallh Elsaeid <124896053+Abduallh-Abdo@users.noreply.github.com>
2026-09-06 16:02:03 +0300

    connectivity done

---
+++ lib/core/app/connectivity_controller.dart
@@ -0,0 +1,26 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
+
+class ConnectivityController {
+  ConnectivityController._();
+
+  static final ConnectivityController instance = ConnectivityController._();
+
+  ValueNotifier<bool> isOnline = ValueNotifier<bool>(true);
+
+  Future<void> init() async {
+    final result = await Connectivity().checkConnectivity();
+    isInternetConnected(result);
+    Connectivity().onConnectivityChanged.listen(isInternetConnected);
+  }
+
+  void isInternetConnected(List<ConnectivityResult> results) {
+ final bool hasConnection = results.any((result) =>
+        result == ConnectivityResult.wifi ||
+        result == ConnectivityResult.mobile ||
+        result == ConnectivityResult.ethernet ||
+        result == ConnectivityResult.vpn);
+
+    isOnline.value = hasConnection;
+  }
+}
