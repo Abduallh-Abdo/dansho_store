75abe78896291befcd46574357b3d6ad459f08ac
Abduallh Elsaeid <124896053+Abduallh-Abdo@users.noreply.github.com>
2026-09-08 15:43:49 +0300

    init text , animate , custom_text_field and custom_linear_button

--- lib/core/styles/fonts/font_family_helper.dart
+++ lib/core/styles/fonts/font_family_helper.dart
@@ -1,6 +1,15 @@
 class FontFamilyHelper {
   const new _();
-  
-  static const String cairoArabic ='Cairo';
-  static const String poppinsEnglish ='Poppins';
+
+  static const String cairoArabic = 'Cairo';
+  static const String poppinsEnglish = 'Poppins';
+
+  static String getLocalazedFontFamily() {
+    final currrentLocale = 'ar';
+    if (currrentLocale == 'ar') {
+      return cairoArabic;
+    } else {
+      return poppinsEnglish;
+    }
+  }
 }
