fbefcffbce8596135817dca2ff1c8a5a13917188
Abduallh Elsaeid <124896053+Abduallh-Abdo@users.noreply.github.com>
2026-09-08 13:00:06 +0300

    lang with flutter intl done

--- lib/dansho_store.dart
+++ lib/dansho_store.dart
@@ -3,7 +3,9 @@ import 'package:dansho_store/core/app/env.variables.dart';
 import 'package:dansho_store/core/common/screens/no_network_screen.dart';
 import 'package:dansho_store/core/routes/app_routes.dart';
 import 'package:dansho_store/core/themes/app_theme.dart';
+import 'package:dansho_store/generated/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class DanshoStore extends StatelessWidget {
@@ -23,13 +25,25 @@ class DanshoStore extends StatelessWidget {
               debugShowCheckedModeBanner: EnvVariable.instance.isDev,
               title: 'Dansho Store',
               theme: themeDark(),
+              locale: const Locale('en'),
+              localizationsDelegates: const [
+                S.delegate,
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+              ],
+              supportedLocales: S.delegate.supportedLocales,
               builder: (context, widget) {
-                return Scaffold(
-                  body: Builder(
-                    builder: (context) {
-                      ConnectivityController.instance.init();
-                      return widget!;
-                    },
+                return GestureDetector(
+                  //! for dismiss keyboard
+                  onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+                  child: Scaffold(
+                    body: Builder(
+                      builder: (context) {
+                        ConnectivityController.instance.init();
+                        return widget!;
+                      },
+                    ),
                   ),
                 );
               },
