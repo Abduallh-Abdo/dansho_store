39202632d79e74a86747d80dd4501dffa6de005b
Abduallh Elsaeid <124896053+Abduallh-Abdo@users.noreply.github.com>
2026-09-19 15:27:54 +0300

    finsh login UI

--- lib/features/auth/presentation/refactors/login_body.dart
+++ lib/features/auth/presentation/refactors/login_body.dart
@@ -1,5 +1,12 @@
+import 'package:dansho_store/core/common/animations/animate_do.dart';
+import 'package:dansho_store/core/common/widgets/text_app.dart';
+import 'package:dansho_store/core/extensions/context_ext.dart';
+import 'package:dansho_store/core/styles/fonts/font_weight_helper.dart';
 import 'package:dansho_store/features/auth/presentation/widgets/auth_title_info.dart';
 import 'package:dansho_store/features/auth/presentation/widgets/dark_and_lang_button.dart';
+import 'package:dansho_store/features/auth/presentation/widgets/login/login_button.dart';
+import 'package:dansho_store/features/auth/presentation/widgets/login/login_text_form.dart';
+import 'package:dansho_store/generated/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -10,14 +17,37 @@ class LoginBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.h),
-      child: const SingleChildScrollView(
+      child: SingleChildScrollView(
         child: Column(
           children: [
             // Dark & lang
-            DarkAndLangButton(),
-            SizedBox(height: 50),
+            const DarkAndLangButton(),
+            const SizedBox(height: 50),
+
             // login title & subtitle
-            AuthTitleInfo(),
+            const AuthTitleInfo(),
+            SizedBox(height: 30.h),
+
+            // login form
+            const LoginTextForm(),
+            SizedBox(height: 30.h),
+
+            // login button
+            const LoginButton(),
+            SizedBox(height: 30.h),
+
+            // Go to Signup Screen Text
+            CustomFadeInDown(
+              duration: 600,
+              child: TextApp(
+                text: S.of(context).create_account,
+                theme: context.textStyle.copyWith(
+                  fontSize: 16.sp,
+                  fontWeight: FontWeightHelper.bold,
+                  color: context.color.bluePinkLight,
+                ),
+              ),
+            ),
           ],
         ),
       ),
---
+++ lib/features/auth/presentation/widgets/login/login_button.dart
@@ -0,0 +1,32 @@
+import 'package:dansho_store/core/common/animations/animate_do.dart';
+import 'package:dansho_store/core/common/widgets/custom_linear_button.dart';
+import 'package:dansho_store/core/common/widgets/text_app.dart';
+import 'package:dansho_store/core/extensions/context_ext.dart';
+import 'package:dansho_store/core/styles/fonts/font_weight_helper.dart';
+import 'package:dansho_store/generated/l10n.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class LoginButton extends StatelessWidget {
+  const new({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomFadeInDown(
+      duration: 500,
+      child: CustomLinearButton(
+        width: double.infinity,
+        height: 44.h,
+        onPressed: () {},
+        child: TextApp(
+          text: S.of(context).login,
+          theme: context.textStyle.copyWith(
+            fontSize: 16.sp,
+            fontWeight: FontWeightHelper.bold,
+            color: context.color.textColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
